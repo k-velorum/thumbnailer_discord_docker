50d396cefe5b01c24245ade2fd77b6c8abd849ed
k-velorum <k.velorum.la@gmail.com>
2024-06-01 22:39:16 +0900

    不要コメント削除

--- token_install.sh
+++ token_install.sh
@@ -1,7 +1,4 @@
 #/bin/bash
-
-# ユーザに「DISCORD BOT TOKENを入力してください」とメッセージを出し、入力を求める
 echo "DISCORD BOT TOKENを入力してください"
 read token
-# TOKENは.env内に、DISCORD_BOT_TOKENの変数名で、docker-composeで利用できるように保存しておく
 echo "DISCORD_BOT_TOKEN=$token" > .env
